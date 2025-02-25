ee9698d7628643b7a9f7c905c21ef23e74134d69
Albedo-momon <rautbhushan6@gmail.com>
2025-02-26 00:00:25 +0530

    new

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -34,15 +34,19 @@ const Hero = () => {
       >
         <div>
           <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-            <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
-              <video ref={nextVideoRef}
+            <div
+              onClick={handleMiniVdClick}
+              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
+            >
+              <video
+                ref={nextVideoRef}
                 src={getVideoSrc(currentIndex + 1)}
                 loop
                 muted
                 id="current-video"
-                className="size-64 origin-center scale-150 object-cover object-center"
+                className="size-64 origin-center scale-150 object-cover"
                 onLoadedData={handleVideoLoad}
-               />
+              />
             </div>
           </div>
         </div>
